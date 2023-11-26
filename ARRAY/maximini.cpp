87622292057be3f8f4197aaa50038a13340960ab
sumit <119105086+sumit-gade@users.noreply.github.com>
2023-11-26 23:56:59 +0530

    Add files via upload

---
+++ ARRAY/maximini.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+/*Pair struct is used to return  2 values*/ 
+
+struct Pair 
+{
+    int min;
+    int max;
+}; 
+ 
+Pair getMinMax(int arr[], int n)
+{
+    struct Pair minmax;     
+    int i;
+     
+    // If there is only one element 
+    
+    if (n == 1)
+    {
+        minmax.max = arr[0];
+        minmax.min = arr[0];     
+        return minmax;
+    } 
+     
+    // If there are more than one elements
+   
+    if (arr[0] > arr[1]) 
+    {
+        minmax.max = arr[0];
+        minmax.min = arr[1];
+    } 
+    else
+    {
+        minmax.max = arr[1];
+        minmax.min = arr[0];
+    } 
+     
+    for(i = 2; i < n; i++)
+    {
+        if (arr[i] > minmax.max)     
+            minmax.max = arr[i];
+             
+        else if (arr[i] < minmax.min)     
+            minmax.min = arr[i];
+    }
+    return minmax;
+}
+ 
+
+int main()
+{
+    int arr[] = {1,2,3,4,5,6 };
+    int arr_size = 6;
+     
+    struct Pair minmax = getMinMax(arr, arr_size);
+     
+    cout << "Minimum element is "
+         << minmax.min << endl;
+    cout << "Maximum element is "
+         << minmax.max;
+          
+    return 0;
+} 
