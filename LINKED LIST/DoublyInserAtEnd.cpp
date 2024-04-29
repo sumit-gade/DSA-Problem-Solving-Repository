122db0bd6fc16056a22d317a08e0c75aa585eb78
SUMIT RANJAN <119105086+sumit-gade@users.noreply.github.com>
2024-04-29 14:23:56 +0530

    Add files via upload

---
+++ LINKED LIST/DoublyInserAtEnd.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+
+
+struct node{
+    int data;
+    node *next;
+    node *prev;//<--
+};
+
+
+
+void printList (node *n){
+   
+    while(n!=NULL){
+        cout<<n->data<<endl;
+        n = n->next;
+        
+    }
+   
+}
+
+/*-------  Insert node in Doubly Linked List  ------- */
+
+void push(node **head_ref, int new_data){
+    
+    node *new_node = new node();
+    new_node->data=  new_data;
+    new_node->next = *head_ref;
+    new_node->prev = NULL;
+    
+    if((*head_ref) != NULL){
+        (*head_ref)->prev = new_node;
+    }
+    
+    *head_ref = new_node;
+}
+
+
+void insertAfter(node *prev_node,int new_data)
+{
+   if(prev_node==NULL)
+   {
+    cout<<"given prev. node cant be null"<<endl;
+    return;
+   }
+
+    node* new_node=new node();
+    new_node->data=new_data;
+    new_node->next=prev_node->next;
+
+
+    prev_node->next=new_node;
+    new_node->prev=prev_node;
+
+    if(new_node->next!=NULL)
+    {
+    new_node->next->prev=new_node;
+    }
+}
+
+void appendd(node **head_ref,int new_data)
+{
+    node *new_node=new node();
+    new_node->data=new_data;
+    new_node->next=NULL;
+
+    node* last= *head_ref;
+
+    if(*head_ref==NULL)
+    {
+        new_node->prev=NULL;
+        *head_ref=new_node;
+        return;
+    }
+
+    while(last->next!=NULL)
+    {
+        last=last->next;
+    }
+
+    last->next=new_node;
+    new_node->prev=last;
+
+    return;
+}
+
+
+int main(){
+    
+    node *head= NULL;
+    push(&head, 4);
+    push(&head, 11);
+    push(&head, -3);
+    push(&head,9);
+    
+    // -3 11 4
+
+    appendd(&head,22);
+    
+    printList(head);
+}
+
